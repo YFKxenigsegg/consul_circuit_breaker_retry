a8a65cb30a6eea96ff0974da510c0c55dea408fb
mattkorrdev <mattkorrdev@gmail.com>
2025-09-17 16:11:05 +0200

    .

--- src/Shared/ServiceDiscovery/IServiceDiscovery.cs
+++
@@ -1,8 +0,0 @@
-namespace CCBR.Shared.ServiceDiscovery;
-
-public interface IServiceDiscovery
-{
-    Task<string> GetServiceUrlAsync(string serviceName);
-    Task RegisterServiceAsync(string serviceName, string serviceUrl, string healthCheckUrl);
-    Task DeregisterServiceAsync(string serviceName, string serviceId);
-}
